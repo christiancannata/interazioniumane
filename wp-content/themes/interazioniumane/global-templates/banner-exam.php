5a3d7b91cd56eec8bb0d540af7cc64172a00934c
Nathalie J. Schwitter <nathalieschwitter@gmail.com>
2023-07-25 11:58:17 +0200

    fix esami

--- wp-content/themes/interazioniumane/global-templates/banner-exam.php
+++ wp-content/themes/interazioniumane/global-templates/banner-exam.php
@@ -3,7 +3,17 @@
     <h4 class="banner__text apply">Iscriviti!</h4>
     <p class="banner__subtitle">Compila il form, partecipa all'esame.</p>
     <div class="banner__form">
-      <?php echo do_shortcode('[contact-form-7 title="Esami"]'); ?>
+      <?php
+        $word = "II livello";
+        $mystring = get_the_title();
+        if (str_contains($mystring, $word)) {
+          echo do_shortcode('[contact-form-7 title="Esami II livello"]');
+        } else{
+            echo do_shortcode('[contact-form-7 title="Esami"]');
+        }
+
+        ?>
+
     </div>
   </div>
 </section>
